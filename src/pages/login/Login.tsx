78f07b925fd04e413ed8aa53a0c3748e52279ca6
bonjun <joonikoo10@gmail.com>
2024-01-24 20:38:16 +0900

    로그인 페이지 레이아웃

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
+
 export const Login = () => {
+  const [id, setId] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const onChangeId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setId(e.target.value)
+  }
+
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+  }
+
+  const onClickLogin = () => {}
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex flex-col items-center space-y-[10px] p-[10px]">
+      <input onChange={onChangeId}></input>
+      <input onChange={onChangePassword}></input>
+      <button className="bg-[#B9D7EA] p-[5px] w-[150px]" onClick={onClickLogin}>
+        로그인
+      </button>
     </div>
   )
 }
